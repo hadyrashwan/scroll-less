ee6be0d0f728837578d2d338e162af01705cc5cc
Hady Rashwan <h2rashwan@gmail.com>
2024-10-03 11:40:41 +0100

    add logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
 
-import { SignIn } from "@/components/sign-in";
-import { auth } from "@/auth";
-import LoggedIn from "@/components/logged-in";
 import Navbar from "@/components/nav-bar";
+import Head from "next/head";
 
 export default async function Home() {
-  const session = await auth();
 
   return (
     <div>
+            <Head>
+        <link rel="png" href="/logos/logo.png" />
+      </Head>
       <Navbar></Navbar>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
